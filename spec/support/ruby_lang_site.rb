fb9cffe575dae569632eae0e84e1e84426b25585
John Fitisoff <jfitisoff@yahoo.com>
2018-04-28 19:56:18 -0700

    badges and ci

--- spec/support/ruby_lang_site.rb
+++ spec/support/ruby_lang_site.rb
@@ -1,6 +1,18 @@
 require 'insite'
 require 'rspec'
 
+require 'coveralls'
+Coveralls.wear!
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+require 'simplecov'
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
 def new_session
   s = RubyLangSite.new 'https://www.ruby-lang.org', language_code: 'en'
   s.open
