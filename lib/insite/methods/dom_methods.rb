80342585170c0fb72875031ded8c123fa13d7606
John Fitisoff <jfitisoff@yahoo.com>
2018-05-10 22:04:31 -0700

    more things and stuff

--- lib/insite/methods/dom_methods.rb
+++ lib/insite/methods/dom_methods.rb
@@ -1,6 +1,6 @@
 module Insite
   module DOMMethods
-    METHOD_MAP.each do |k, v|
+    Insite::METHOD_MAP.each do |k, v|
       # Create class-level methods for defining class-level methods for defining
       # DOM elements (1:1 match for Watir browser DOM methods.)
       v.each do |mth|
@@ -8,28 +8,48 @@ module Insite
         define_method(mth) do |name=nil, *args, &block|
           @page_elements ||= [name.to_sym]
 
+          # For now, do some mumbo jumbo to determine the right class name and
+          # look at handling in the method map a little later.
+          class_str = Insite::METHOD_MAP.find do |k, v|
+            Insite::METHOD_MAP[k].include?(mth)
+          end.first.to_s.demodulize
+
+          # If a block is given when making the class-level method call to define
+          # the page element, create a new, modified version of the element class
+          # that's nested within the page class.
+          if block_given?
+            klass = Class.new("Insite::#{class_str}".constantize) do
+              self.class_eval(&block) if block_given?
+            end
+
+            # cname = name.to_s.camelcase
+            puts "block_given: #{self}"
+            self.const_set(name.to_s.camelcase, klass) unless self.const_defined? name.to_s.camelcase
+          end
+
           # Instance method that's generated on the object when the class-level
           # method is invoked.
-          define_method(name) do |*args, &block|
-            class_str = METHOD_MAP.find do |k, v|
-              METHOD_MAP[k].include?(mth)
-            end.first.to_s.demodulize
-# TODO: In cases where a block is defined a customized element class should be
-# created here and nested within the page object. Then an accessor method should
-# be created for the customized element.
-
-            "Insite::#{class_str}".constantize.new(
-              @site,
-              @browser.send(*args.unshift(mth)),
-              &block
-            )
+          if block_given?
+            define_method(name) do |*args, &block|
+              name.to_s.camelcase.constantize.new(
+                @site,
+                @browser.send(*args.unshift(mth))
+              )
+            end
+          else
+            define_method(name) do |*args, &block|
+              "Insite::#{class_str}".constantize.new(
+                @site,
+                @browser.send(*args.unshift(mth))
+              )
+            end
           end
         end
       end
 
-     # TODO: (More context when this happens.)
-     # ArgumentError: wrong number of arguments (given 1, expected 0)
-     # from /Users/john/.rbenv/versions/2.3.5/lib/ruby/gems/2.3.0/gems/insite-0.5.1/lib/insite/methods/dom_methods.rb:17:in `block in el'
+      # TODO: (More context when this happens.)
+      # ArgumentError: wrong number of arguments (given 1, expected 0)
+      # from /Users/john/.rbenv/versions/2.3.5/lib/ruby/gems/2.3.0/gems/insite-0.5.1/lib/insite/methods/dom_methods.rb:17:in `block in el'
       def el(name, &block)
         @page_elements ||= []
         @page_elements << name.to_sym
