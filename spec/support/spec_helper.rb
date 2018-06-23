f63ceed5d3bf4d98e2efd896ebc3269d8cc98993
John Fitisoff <jfitisoff@yahoo.com>
2018-06-23 13:46:32 -0700

    code cov

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -3,8 +3,11 @@ if ENV['CIRCLE_ARTIFACTS']
   require 'simplecov'
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
-  SimpleCov.start
-
+  # SimpleCov.start
+  SimpleCov.start do
+    add_filter 'spec'
+    add_group  'lib'
+  end
   require 'coveralls'
   Coveralls.wear_merged!
 end
