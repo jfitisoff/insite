35a7b91abad90f2900a8d81950f92fc9b012ebdd
John Fitisoff <jfitisoff@yahoo.com>
2018-07-02 11:19:00 -0700

    coverage settings

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -1,16 +1,33 @@
-if ENV['CIRCLE_ARTIFACTS']
-  require 'coveralls'
-  Coveralls.wear_merged!
+require 'simplecov'
+dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
 
-  # save to CircleCI's artifacts directory if we're on CircleCI
-  require 'simplecov'
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
 
-  SimpleCov.start do
-    coverage_dir dir
-    add_filter   'spec', 'examples'
-  end
+SimpleCov.coverage_dir(dir)
+# SimpleCov.start
+SimpleCov.start do
+  add_filter 'spec', 'examples'
+  add_group  'lib'
 end
+
+require 'coveralls'
+Coveralls.wear_merged!
+
+
+# # Most recent
+# if ENV['CIRCLE_ARTIFACTS']
+#   require 'coveralls'
+#   Coveralls.wear_merged!
+#
+#   # save to CircleCI's artifacts directory if we're on CircleCI
+#   require 'simplecov'
+#   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+#
+#   SimpleCov.start do
+#     coverage_dir dir
+#     add_filter   'spec', 'examples'
+#   end
+# end
+
 
 # require 'coveralls'
 # Coveralls.wear!
