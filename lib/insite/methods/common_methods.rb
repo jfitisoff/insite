edb85e72a4fedbca79a28e5e8c843d010a7b6820
John Fitisoff <jfitisoff@yahoo.com>
2018-10-13 00:08:36 -0700

    some changes to get update_object working better with all of the component changes.

--- lib/insite/methods/common_methods.rb
+++ lib/insite/methods/common_methods.rb
@@ -49,46 +49,6 @@ module Insite
 
     end
     public
-    # private
-    # def process_browser
-    #   if @site.browser.is_a?(Watir::Browser)
-    #     begin
-    #       if @site.browser.exists?
-    #         return @site.browser
-    #       else
-    #         raise(
-    #           Insite::Errors::BrowserClosedError,
-    #           "Browser check failed. The browser is no longer present.\n\n"
-    #         )
-    #       end
-    #     rescue(Insite::Errors::BrowserNotOpenError) => e
-    #       raise e
-    #     rescue => e
-    #       raise(
-    #         Insite::Errors::BrowserResponseError,
-    #         <<~eos
-    #         Browser check failed. The browser returned an #{e.class} (#{e}) when it was queried.
-    #         Backtrace for the error:
-    #         #{e.backtrace.join("\n")}
-    #
-    #         eos
-    #       )
-    #     end
-    #   elsif @site.browser.nil?
-    #     raise(
-    #       Insite::Errors::BrowserNotOpenError,
-    #       "A browser has not been defined for the site. Try using Site#open to " \
-    #       "start a browser.\n\n"
-    #     )
-    #   else
-    #     raise(
-    #       Insite::Errors::BrowserNotValidError,
-    #       "Expected: Watir::Browser. Actual: #{@site.browser.class}.\n\n"
-    #     )
-    #   end
-    #
-    # end
-    # public
 
     def update_object(hash_args = {})
       rescues = [
@@ -99,12 +59,20 @@ module Insite
       ]
       failed = []
       hash_args.each do |k, v|
+        sleep 0.2
         begin
-          k = k.to_sym
-          if @page_elements.include?(k)
-            elem = public_send(k)
-
-            if [Insite::Alert, Insite::FileField, Insite::TextField, Insite::TextArea].include? elem.class
+          # k = k.to_sym
+          # if self.is_a?(Component)
+          #   elems = @component_elements
+          # else
+          #   elems = @page_elements
+          # end
+          #
+          # # TODO: DRY
+          # if self.is_a?(Insite::DefinedPage) && @page_elements.include?(k)
+          elem = public_send(k)
+          if elem.is_a?(Insite::Element)
+            if [Insite::FileField, Insite::TextField, Insite::TextArea].include? elem.class
               elem.set v
             elsif [Insite::Select].include? elem.class
               elem.select v
@@ -167,11 +135,16 @@ module Insite
                 raise ArgumentError, "Unsupported argument for #{elem.class}: '#{v}'"
               end
             end
-          elsif @component_elements.include?(k)
+          elsif elem.is_a?(Component) #&& @component_elements.include?(k)
+
             w = public_send(k)
 
             begin
-              w.update(v)
+              if w.is_a?(Component) && v.is_a?(Hash)
+                w.update_object(**v)
+              elsif w.is_a?(Component) && w.respond_to?(:update)
+                w.update(v)
+              end
             rescue => e
               begin
                 if v.is_a?(Array)
@@ -198,7 +171,11 @@ module Insite
                   public_send(k).update(*v)
                 end
               rescue => e2
-                raise ArgumentError, "Dynamic method call failed for #{k}.", e2.backtrace
+                raise(
+                  ArgumentError,
+                  "Dynamic method call failed for #{k}: #{e2.try(:message)}",
+                  e2.backtrace
+                )
               end
             end
           end
@@ -211,7 +188,7 @@ module Insite
               redo
             end
           else
-            raise e, "Failure trying to update #{k} with #{v.class}: #{v}:\n" + e.backtrace.join("\n")
+            raise e, "Failure trying to update #{k} with #{v.class}: #{v}. Original message: #{e.try(:message)}\n" + e.backtrace.join("\n")
           end
         end
         sleep 0.2
